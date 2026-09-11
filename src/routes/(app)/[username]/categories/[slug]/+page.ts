1c1c9cded0c37873c6c2cff15db154a2aff10e7c
Nato Boram <nato.boram+git@gmail.com>
2026-09-11 02:18:11 -0400

    🐛 Yo pass the data
    Signed-off-by: Nato Boram <nato.boram+git@gmail.com>

--- src/routes/(app)/[username]/categories/[slug]/+page.ts
+++ src/routes/(app)/[username]/categories/[slug]/+page.ts
@@ -1,8 +1,8 @@
 import { parseMarkdown } from '$lib/markdown.js'
 import type { PageLoad } from './$types.ts'
 
-export const load: PageLoad = (async ({ parent }) => {
+export const load: PageLoad = (async ({ parent, data: { items } }) => {
 	const { category } = await parent()
 	const description = await parseMarkdown(category.description)
-	return { description }
+	return { description, items }
 }) satisfies PageLoad
